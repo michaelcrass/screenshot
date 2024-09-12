66a52778cc0c6aa3078a4b318fb3563720171c29
Michael Crass <michaelcrass36@gmail.com>
2024-09-12 19:43:31 +0200

    pyinstaller demon

--- exe_pyinstaller.py
+++ exe_pyinstaller.py
@@ -1,12 +1,7 @@
 import PyInstaller.__main__
 
-# PyInstaller.__main__.run([
-#     'excel_backup_gdrive.py',
-#     '--onefile',
-#     '-w'#als demon = ohne shell
-# ])
-
 PyInstaller.__main__.run([
     'screenshot.py',
-    '--onefile'
+    '--onefile', # als one file 
+    '-w'#als demon = ohne shell
 ])
