1c43636210e33cd7aa24d692ad7386d235542d92
Michael Crass <michaelcrass36@gmail.com>
2024-09-23 12:49:09 +0200

    kompilieren_exe

--- kompilieren_exe.py
+++ kompilieren_exe.py
@@ -4,5 +4,6 @@ PyInstaller.__main__.run([
     'screenshot-tool.py',
     '--onefile', # als one file 
     # '--onedir', # in one subfolder
+    '--icon=icon.ico',
     '-w'#als demon = ohne shell
 ])
